16337612527df0ca539ed72dd326f7a199857ad7
phanimadiraju <98398051+phanimadiraju@users.noreply.github.com>
2022-02-27 22:34:51 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@
 
 function App() {
   return (
-    <div >
+    <div className="header">
       Phone directory
+      <button>Add</button>
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input id="name" type="text" placeholder="Type here" defualtValue="Phani"/>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
