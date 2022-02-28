7262415ceb635c4d39c01043004a84b63aa59421
phanimadiraju <98398051+phanimadiraju@users.noreply.github.com>
2022-02-28 19:04:26 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
-
+import Header from './Header';
 function App() {
   return (
-    <div className="header">
-      Phone directory
-      <button>Add</button>
+    <div>
+      <Header/>
+    <button>Add</button>
       <div>
-        <label htmlFor="name">Name: </label>
-        <input id="name" type="text" placeholder="Type here" defualtValue="Phani"/>
         <span>Name</span><br />
         <span>Phone</span>
       </div>
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header=function(){
+
+    return(
+        <div className="header">
+      Phone directory
+    </div>
+    )
+    
+}
+
+export default Header;
